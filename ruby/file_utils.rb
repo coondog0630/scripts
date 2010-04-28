ee04a48ced417e688fb13400bf7c50a33634f440
Sahil R Cooner <sahil.cooner@gmail.com>
2010-04-27 21:40:04 -0700

    let's put the code in the right place ;).
    Signed-off-by: Sahil R Cooner <sahil.cooner@gmail.com>

--- ruby/file_utils.rb
+++ ruby/file_utils.rb
@@ -1,28 +1,29 @@
 #!/usr/bin/env ruby
 
 class File
+  
+=begin rdoc
+  The +same_contents+ method compares to files at block level.
+=end
   def File.same_contents(p1, p2)
+    # Performing basic checks
     return false if File.exists?(p1) != File.exists?(p2)
     return true if !File.exists?(p1)
     return true if File.expand_path(p1) == File.expand_path(p2)
     return false if File.ftype(p1) != File.ftype(p2) || 
       File.size(p1) != File.size(p2)
-  end
-end
-
-def main
-  open(p1) { |f1|
-    open(p2, "w") { |f2| 
-      blocksize = f1.lstat.blksize
-      same = true
-      while same && !f1.eof? && !f2.eof?
-        same = f1.read(blocksize) == f2.read(blocksize)
-      end
-      return same
+      
+    # Compare the files by block
+    open(p1) { |f1|
+      open(p2, "w") { |f2| 
+        blocksize = f1.lstat.blksize
+        same = true
+        while same && !f1.eof? && !f2.eof?
+          same = f1.read(blocksize) == f2.read(blocksize)
+        end
+        return same
+      }
     }
-  }
+    end
   end
 end
-
-# Testing
-main
