536ef5ddcb76807eaa42efe63560ea34051f9b52
Sahil Cooner <sahil.cooner@gmail.com>
2011-06-17 08:10:50 -0700

    quick synergy scripts.

---
+++ bash/synergy_server_home.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/Applications/synergy-1.4.2-MacOSX106-Universal/synergys --config ~/.synergy-home.conf --log ~/.synergy-home.log --no-restart --debug INFO --address 192.168.3.2 --daemon
